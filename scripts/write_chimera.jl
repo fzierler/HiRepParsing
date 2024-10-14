81dd422762a672e3388073dad1bbd554c16c72a7
Fabian Zierler <fabian.zierler@a1.net>
2024-10-14 11:14:09 +0100

    provide list of channels for chimera baryon; update to julia 1.11

--- scripts/write_chimera.jl
+++ scripts/write_chimera.jl
@@ -7,11 +7,14 @@ using HDF5
 # It creates a single hdf5 file for all log files. Measurements performed on the same ensemble
 # are written in distinct hdf5 groups labelled  by the variable `ensemble`
 
-dir = expanduser("~/Documents/DataDiaL/LSD")
-h5file = expanduser("~/Downloads/chimera_data.hdf5")
+dir = expanduser("/home/fabian/Downloads/Chimera_Data_new/logs/")
+h5file = expanduser("~/Downloads/chimera_data_reduced_new.hdf5")
 use_regex_parsing = false
 
-function main(dir,h5file)
+filter_channels=true
+channels=readlines("input/channels_chimera.txt")
+
+function main(dir,h5file;setup=true,filter_channels=false,channels=nothing)
 
     # I have defined a reference variable for the last saved ensemble.
     # If the ensemble changes, we save also information on the lattice setup (coupling, size, bare masses) 
@@ -34,30 +37,32 @@ function main(dir,h5file)
         N1 = parse(Int,m[:N1])
         N2 = parse(Int,m[:N2])
         name  = first(splitext(basename(file)))
-        types = ["source_N$(N1)_sink_N$N" for N in 0:10:N2]
+        types = ["source_N$(N1)_sink_N$N" for N in 0:40:80]
         
         # parse the ensemble name from the filename 
         # (again this depends strongly on the naming scheme)
         # Check if we need to write the lattice-setupparameters to hdf5 file
         ensemble = replace(name,"N$(N1)_N$(N2)"=>"")
-        setup     = ensemble != ensemble0
+        if !isempty(ensemble0)
+            setup     = ensemble != ensemble0
+        end
         ensemble0 = ensemble
         @show ensemble, setup 
               
-        #####################################################
-        # Method A: Using a list of types                   #
-        #####################################################
         ###################################################################################
-        # Method B: Using a regular expression directly  (faster, but it uses more ram)   #
+        # Method A: Using a regular expression directly  (faster, but it uses more ram)   #
         ###################################################################################
+        #####################################################
+        # Method B: Using a list of types                   #
+        #####################################################
         if use_regex_parsing
-            regex = r"source_N[0-9]+_sink_N[0-9]+"
-            writehdf5_spectrum_with_regexp(file,h5file,regex;mixed_rep=true,h5group=ensemble,setup)
+            regex = r"source_N[0-9]+_sink_N[0,40,80]"
+            writehdf5_spectrum_with_regexp(file,h5file,regex;mixed_rep=true,h5group=ensemble,setup,filter_channels,channels)
         else
-            writehdf5_spectrum(file,h5file,types;mixed_rep=true,h5group=ensemble,setup)
+            writehdf5_spectrum(file,h5file,types;mixed_rep=true,h5group=ensemble,setup,filter_channels,channels)
         end
 
     end
 end
 
-main(dir,h5file)
+main(dir,h5file;filter_channels,channels)
