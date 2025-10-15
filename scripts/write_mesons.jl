15e477823966239a50c791d91a48181e54b76616
Fabian Zierler <go46nip@ph.tum.de>
2025-10-15 17:04:21 +0200

    remove HiRepOutputCleaner as dependency

--- scripts/write_mesons.jl
+++ scripts/write_mesons.jl
@@ -2,7 +2,6 @@ using Pkg; Pkg.activate("."); Pkg.instantiate()
 using HiRepParsing
 using HDF5
 using DelimitedFiles
-using HiRepOutputCleaner
 
 # This script parses the log files in the directory 'dir', and saves them as an hdf5-file 
 # in the location provided by 'h5file'.
@@ -13,16 +12,12 @@ function main(listfile,h5file;setup=true,filter_channels=false,channels=nothing)
     isfile(h5file) && rm(h5file)
     for file in readdlm(listfile)
         @show file
-        # use HiRepOutputCleaner by default
-        tmp_filename = "tmp.txt"
-        clean_hirep_file(file,tmp_filename;checkpoint_pattern="analysed")
         # only try parsing if the filesize is non-vanishing
-        if filesize(tmp_filename) > 0
+        if filesize(file) > 0
             ensemble = match(r"Lt[0-9]+Ls[0-9]+beta[0-9]+.[0-9]+mas-[0-9]+.[0-9]+FUN",file).match
             smearing_regex = r"source_N[0-9]+_sink_N[0-9]+"
-            writehdf5_spectrum_with_regexp(tmp_filename,h5file,smearing_regex;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
+            writehdf5_spectrum_with_regexp(file,h5file,smearing_regex;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
         end
-        rm(tmp_filename)
     end
 end
 main(ARGS[1],ARGS[2])
