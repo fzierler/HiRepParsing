5ef4d8de751524dfab4e2462ac254bab0f9b7440
Fabian Zierler <fabian.zierler@a1.net>
2025-12-04 14:21:16 +0100

    fix regular expressions

--- scripts/write_mesons_glueballs.jl
+++ scripts/write_mesons_glueballs.jl
@@ -6,14 +6,15 @@ using DelimitedFiles
 function main(listfile,h5file;setup=true,filter_channels=false,channels=nothing)
     isfile(h5file) && rm(h5file)
     for (ensemble,disc,nhits,file) in eachrow(readdlm(listfile,','))
-        smearing_regex = r"source_N[0-9]+_sink_N[0-9]+"
         if disc == "disc"
+            smearing_regex = r"DISCON_SEMWALL smear_N[0-9]+ SINGLET "
             writehdf5_spectrum_disconnected_with_regexp(file,h5file,smearing_regex,nhits;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
         else
+            smearing_regex = r"source_N[0-9]+_sink_N[0-9]+ TRIPLET "
             writehdf5_spectrum_with_regexp(file,h5file,smearing_regex;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
         end
 
     end
 end
 
-main("input/listfile.txt","test.hdf5",filter_channels=true,channels=readdlm("input/channels_for_glueballs"))
+main("input/listfile.txt","test.hdf5",filter_channels=false,channels=nothing)
