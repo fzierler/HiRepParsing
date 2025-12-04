93a983f48a4e0ab132e5a8478c07c13362673b3f
Fabian Zierler <fabian.zierler@a1.net>
2025-12-04 13:18:25 +0100

    add script for parsing glueballs

---
+++ scripts/write_mesons_glueballs.jl
@@ -0,0 +1,19 @@
+using Pkg; Pkg.activate("."); Pkg.instantiate()
+using HiRepParsing
+using HDF5
+using DelimitedFiles
+
+function main(listfile,h5file;setup=true,filter_channels=false,channels=nothing)
+    isfile(h5file) && rm(h5file)
+    for (ensemble,disc,nhits,file) in eachrow(readdlm(listfile,','))
+        smearing_regex = r"source_N[0-9]+_sink_N[0-9]+"
+        if disc == "disc"
+            writehdf5_spectrum_disconnected_with_regexp(file,h5file,smearing_regex,nhits;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
+        else
+            writehdf5_spectrum_with_regexp(file,h5file,smearing_regex;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
+        end
+
+    end
+end
+
+main("input/listfile.txt","test.hdf5")
